094eb0f53ffa02b0fe3baed2dbc31d10ddf98b14
Roi Eclarin <roi.eclarin@scotiabank.com>
2019-01-02 20:23:09 -0500

    added in memory implementation

--- pkg/entity/user.go
+++ pkg/entity/user.go
@@ -1,8 +1,11 @@
 package entity
 
+import "time"
+
 // User entity
 type User struct {
-	UserID   string `json:"userid"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
+	UserID    string    `json:"userid"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
 }
--- pkg/user/repository_inmem.go
+++ pkg/user/repository_inmem.go
@@ -4,7 +4,7 @@ import (
 	"github.com/surefire1982/exampleservice/pkg/entity"
 )
 
-// IRepo in memory repository
+// IRepo in-memory repository
 type IRepo struct {
 	userMap map[string]*entity.User
 }
@@ -21,4 +21,32 @@ func NewInMemRepository() *IRepo {
 func (repo *IRepo) Store(user *entity.User) (string, error) {
 	repo.userMap[user.UserID] = user
 	return user.UserID, nil
+}
+
+// Find a user
+func (repo *IRepo) Find(id string) (*entity.User, error) {
+	if repo.userMap[id] == nil {
+		return nil, entity.ErrNotFound
+	}
+	return repo.userMap[id], nil
+}
+
+// Delete a user
+func (repo *IRepo) Delete(id string) error {
+	if repo.userMap[id] == nil {
+		return entity.ErrNotFound
+	}
+
+	delete(repo.userMap, id)
+	return nil
+}
+
+// FindAll Users
+func (repo *IRepo) FindAll() ([]*entity.User, error) {
+	var users []*entity.User
+	for _, j := range repo.userMap {
+		users = append(users, j)
+	}
+
+	return users, nil
 }
